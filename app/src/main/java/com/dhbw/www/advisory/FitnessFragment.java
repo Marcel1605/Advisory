b9e10f3eb5478499c89baf43d9903a7214ce8ecd
Magnus Luttmann <magnus@dieluttmanns.de>
2017-05-30 19:37:05 +0200

    Fragments hinzugefügt

---
+++ app/src/main/java/com/dhbw/www/advisory/FitnessFragment.java
@@ -0,0 +1,34 @@
+package com.dhbw.www.advisory;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.
+
+/**
+ * Created by Magnus on 30.05.17.
+ */
+
+@android.support.annotation.RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+public class FitnessFragment extends Fragment {
+    public FitnessFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fitness, container, false);
+    }
+
+    @Override
+    public void onPause(){
+        //The system calls this method as the first indication that the user is leaving the fragment (though it does not always mean the fragment is being destroyed). This is usually where you should commit any changes that should be persisted beyond the current user session (because the user might not come back).
+        Log.e("DEBUG", "OnPause of loginFragment");
+        super.onPause();
+    }
+}
