823aa645b5e57d60f7d361304f459b2433892723
Marcel Keller <marcel.keller1605@gmail.com>
2017-06-14 15:00:45 +0200

    update

--- app/src/main/java/de/dhbw/advisory/GymFragment.java
+++ app/src/main/java/de/dhbw/advisory/GymFragment.java
@@ -90,6 +90,105 @@ public class GymFragment extends Fragment {
        super.onPause();
     }
 
+    /**
+     * Klasse für den Hintergrund Thread
+     */
+    public static class GymAPI extends AsyncTask<Object, Object, Void> {
+
+        protected Context context;
+
+        GymAPI(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected Void doInBackground(Object... params) {
+            try{
+                GPSBestimmung gps = new GPSBestimmung(context);
+                Log.i("GymFragment", "setPosition gestartet");
+                gps.setPosition();
+                getGyms("gym" ,gps);
+
+            } catch(Exception e) {
+               Log.i("GymFragment:","doInBackground Test");
+            }
+            return null;
+        }
+
+
+
+        /**
+         *
+         * Die Methode macht die HTTP Anfrage an die API
+         *
+         * @param
+         * @return gibt den HTTP Request zurück
+         * @throws Exception
+         */
+        protected String getGyms(String typ, GPSBestimmung gps) throws Exception {
+            try {
+                Log.i("GymFragment: ","Methode begonnen");
+                Log.i("GymFragment: ","Übergebener Wert: " + typ);
+                Log.i("GymFragment: ","Starte API Aufruf");
+
+                //1. Request Factory holen
+                Log.i("GymFragment: ","Request Factory holen begonnen");
+                HttpTransport httpTransport = new NetHttpTransport();
+                HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+                Log.i("GymFragment: ","Request Factory holen beendet");
+
+
+                //2. Url hinzufügen
+                Log.i("GymFragment: ","URL hinzufügen begonnen");
+                GenericUrl url = new GenericUrl("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                url.put("location", + gps.getBreitengrad() + "," + gps.getLaengengrad());
+                url.put("rankby", "distance");
+                url.put("typ", typ);
+                url.put("key", "AIzaSyDXzj9XCSY5FtsEyvtKaOtxctEX7ybzrpU");
+                Log.i("GymFragment: ","URL hinzufügen beendet: " + url);
+
+
+                //3. Request absetzen
+                Log.i("GymFragment: ","Request absetzen begonnen");
+                HttpRequest request = requestFactory.buildGetRequest(url);
+                Log.i("GymFragment: ","Request absetzen");
+                HttpResponse httpResponse = request.execute();
+                Log.i("GymFragment: ","Request absetzen beendet");
+
+                Log.i("GymFragment: ","Request parsen");
+                String jsonResponseString = httpResponse.parseAsString();
+
+                Log.i("GymFragment: ","API Aufruf beendet");
+                Log.i("GymFragment: ","Erhaltene JSON: " + jsonResponseString);
+                return jsonResponseString;
+            } catch (Exception e) {
+                Log.i("Fehler GymFragment", "Request absetzen fehlgeschlagen. Fehlermeldung " + e);
+                return "error";
+            }
+        }
+
+        /**
+         *
+         * Diese Methode parsed die Http Antwort der RecipeSearch Methode
+         *
+         * @param jsonString RecipeSearch Http Antwort als String parsen
+         * @return
+         * @throws JSONException
+         */
+        protected ArrayList parseRecipe(String jsonString) throws JSONException {
+            try{
+
+            } catch (Exception e) {
+                //Fehler Exception
+
+                return new ArrayList();
+            }
+
+            return null;
+        }
+
+
+    }
 }
 
 
